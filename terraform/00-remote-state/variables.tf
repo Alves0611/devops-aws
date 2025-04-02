dc150d48d491e9a096831710790705c78fbd226a
“Gabriel <g.alves0611@gmail.com>
2025-04-02 07:10:03 -0300

    feat: add state lock config variable for DynamoDB backend

--- terraform/00-remote-state/variables.tf
+++ terraform/00-remote-state/variables.tf
@@ -11,3 +11,20 @@ variable "tags" {
   }
   description = "Tags to be applied to all resources"
 }
+
+variable "state_lock_config" {
+  type = object({
+    dynamodb_billing_mode            = string
+    dynamodb_hash_key_attribute_name = string
+    dynamodb_hash_key_attribute_type = string
+  })
+
+  default = {
+    dynamodb_billing_mode            = "PAY_PER_REQUEST"
+    dynamodb_hash_key_attribute_name = "LockID"
+    dynamodb_hash_key_attribute_type = "S"
+  }
+
+  description = "Configuration for the DynamoDB table used to lock the Terraform remote state"
+}
+
