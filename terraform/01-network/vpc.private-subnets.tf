f560b5735fae1ba4b34d312b8639c091a39c1fa8
“Gabriel <g.alves0611@gmail.com>
2025-04-02 08:43:17 -0300

    feat: associate private subnets with private route table

--- terraform/01-network/vpc.private-subnets.tf
+++ terraform/01-network/vpc.private-subnets.tf
@@ -8,3 +8,10 @@ resource "aws_subnet" "private" {
 
   tags = { Name = "${var.vpc.name}-${var.vpc.private_subnets[count.index].name}" }
 }
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
