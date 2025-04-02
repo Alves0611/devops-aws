5dc448a856537dd10ecf58dd014053533d134b81
“Gabriel <g.alves0611@gmail.com>
2025-04-02 08:33:15 -0300

    feat: add public and private subnets to VPC variable

--- terraform/01-network/variables.tf
+++ terraform/01-network/variables.tf
@@ -17,11 +17,47 @@ variable "vpc" {
     name                  = string
     cidr_block            = string
     internet_gateway_name = string
+    public_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
+    private_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
   })
 
   default = {
     name                  = "studying"
     cidr_block            = "10.0.0.0/24"
     internet_gateway_name = "igw"
+    public_subnets = [{
+      name                    = "public-1a"
+      cidr_block              = "10.0.0.0/26"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = true
+      },
+      {
+        name                    = "public-1b"
+        cidr_block              = "10.0.0.64/26"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = true
+    }]
+    private_subnets = [{
+      name                    = "private-1a"
+      cidr_block              = "10.0.0.128/26"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = false
+      },
+      {
+        name                    = "private-1b"
+        cidr_block              = "10.0.0.192/26"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = false
+    }]
   }
 }
