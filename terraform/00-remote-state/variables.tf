cd771d222d032488914c4d91b9518a9872dcf762
“Gabriel <g.alves0611@gmail.com>
2025-04-02 06:55:14 -0300

    feat: add tags variable with default managed-by tag

--- terraform/00-remote-state/variables.tf
+++ terraform/00-remote-state/variables.tf
@@ -3,3 +3,11 @@ variable "region" {
   default     = "us-east-1"
   description = "The AWS region where the resources will be provisioned"
 }
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Managedby = "Terraform"
+  }
+  description = "Tags to be applied to all resources"
+}
