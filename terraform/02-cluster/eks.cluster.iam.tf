99d55337129967367e0a6531c3c0d877a8206862
“Gabriel <g.alves0611@gmail.com>
2025-04-03 08:49:14 -0300

    feat: add IAM role for EKS cluster with assume role policy

---
+++ terraform/02-cluster/eks.cluster.iam.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = var.eks_cluster.role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
