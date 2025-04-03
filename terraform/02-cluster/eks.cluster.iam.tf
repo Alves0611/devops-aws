2db135caec67b0a517ae47cff84164254afb9713
“Gabriel <g.alves0611@gmail.com>
2025-04-03 08:49:30 -0300

    feat: attach AmazonEKSClusterPolicy to EKS cluster IAM role

--- terraform/02-cluster/eks.cluster.iam.tf
+++ terraform/02-cluster/eks.cluster.iam.tf
@@ -16,3 +16,8 @@ resource "aws_iam_role" "eks_cluster" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
