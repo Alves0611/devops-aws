478421b48727743fe46b9dd87797bc20efcdb220
“Gabriel <g.alves0611@gmail.com>
2025-04-03 08:56:47 -0300

    feat: attach required IAM policies to EKS node group role

--- terraform/02-cluster/eks.cluster.node-group.iam.tf
+++ terraform/02-cluster/eks.cluster.node-group.iam.tf
@@ -12,4 +12,17 @@ resource "aws_iam_role" "eks_node_group" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
 
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group.name
+}
