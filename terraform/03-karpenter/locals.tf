a7d54430854a8f43aa477ec77c99c91b44847b0e
“Gabriel <g.alves0611@gmail.com>
2025-04-05 13:24:54 -0300

    chore: add local for EKS cluster security group ID from remote state

--- terraform/03-karpenter/locals.tf
+++ terraform/03-karpenter/locals.tf
@@ -2,4 +2,5 @@ locals {
   eks_oidc_url         = replace(data.terraform_remote_state.eks.outputs.oidc_provider_url, "https://", "")
   node_group_role_name = data.terraform_remote_state.eks.outputs.eks_node_group_role_name
   eks_cluster_name     = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  eks_cluster_sg_id    = data.terraform_remote_state.eks.outputs.eks_cluster_security_group_id
 }
