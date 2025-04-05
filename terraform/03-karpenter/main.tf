f95e9c07d841258bec30208820452e597a4fb97a
“Gabriel <g.alves0611@gmail.com>
2025-04-05 14:26:38 -0300

    feat: configure Helm provider to deploy charts into EKS cluster

--- terraform/03-karpenter/main.tf
+++ terraform/03-karpenter/main.tf
@@ -17,3 +17,14 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = local.eks_endpoint
+    cluster_ca_certificate = base64decode(local.eks_ca_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", local.eks_cluster_name]
+      command     = "aws"
+    }
+  }
+}
