7addc23b73ae2a6acc60c43cc51f44146fcc4f3f
“Gabriel <g.alves0611@gmail.com>
2025-04-05 13:45:57 -0300

    chore: add EKS cluster endpoint and CA data to locals

--- terraform/03-karpenter/locals.tf
+++ terraform/03-karpenter/locals.tf
@@ -3,4 +3,6 @@ locals {
   node_group_role_name = data.terraform_remote_state.eks.outputs.eks_node_group_role_name
   eks_cluster_name     = data.terraform_remote_state.eks.outputs.eks_cluster_name
   eks_cluster_sg_id    = data.terraform_remote_state.eks.outputs.eks_cluster_security_group_id
+  eks_endpoint         = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  eks_ca_data          = data.terraform_remote_state.eks.outputs.eks_cluster_ca_data
 }
